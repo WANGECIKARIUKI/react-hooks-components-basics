a38dadf3be813151ef8dfc230f1fd3370d4300ee
LUCY <wangecikariu.luc@gmail.com>
2023-08-20 15:42:19 +0300

    Hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Article from "./Article";
 import Comment from "./Comment";
+import Feedback from "./Feedback";
+
 
 function App() {
   return (
     <div>
       <Article />
       <Comment />
+      <Feedback />
     </div>
   );
 }
---
+++ src/Feedback.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Feedback() {
+  return <div>This article is awesome.</div>;
+}
+
+export default Feedback;
